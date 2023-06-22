9758fbbf0ccf019cc31b275f5d44cff32c05973b
numinex <g.koszyk98@gmail.com>
2023-06-22 16:56:35 +0200

    Updated Order Messages.

--- Playground/Orders.cs
+++ Playground/Orders.cs
@@ -1,25 +1,25 @@
 namespace ConsoleApp;
 public class OrderCreated
 {
-	public ulong Id { get; set; }
-	public string CurrencyPair { get; set; }
-	public double Price { get; set; }
-	public double Quantity { get; set; }
-	public string Side { get; set; }
-	public ulong Timestamp { get; set; }
+	public ulong Id { get; init; }
+	public string CurrencyPair { get; init; }
+	public double Price { get; init; }
+	public double Quantity { get; init; }
+	public string Side { get; init; }
+	public ulong Timestamp { get; init; }
 }
 
 public class OrderConfirmed
 {
-	public ulong Id { get; set; }
-	public double Price { get; set; }
-	public ulong Timestamp { get; set; }
+	public ulong Id { get; init; }
+	public double Price { get; init; }
+	public ulong Timestamp { get; init; }
 }
 
 public class OrderRejected
 {
-	public ulong Id { get; set; }
-	public ulong Timestamp	{ get; set; }
-	public string Reason { get; set; }
+	public ulong Id { get; init; }
+	public ulong Timestamp	{ get; init; }
+	public string Reason { get; init; }
 }
 
