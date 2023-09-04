d30923f9da73f5164eb405ac4096c8d7819e5226
numinex <g.koszyk98@gmail.com>
2023-09-04 20:37:17 +0200

    Fixed duplicated methods in StreamFactory.cs

--- Iggy_SDK_Tests/Utils/Streams/StreamFactory.cs
+++ Iggy_SDK_Tests/Utils/Streams/StreamFactory.cs
@@ -24,23 +24,6 @@ internal static class StreamFactory
             StreamId = Random.Shared.Next(1, 10),
         };
     }
-
-    internal static StreamResponse CreateStreamResponse()
-    {
-        var responses = new List<TopicResponse>();
-        var topicResponse = TopicFactory.CreateTopicsResponse();
-        responses.Add(topicResponse);
-        return new StreamResponse
-        {
-            Id = Random.Shared.Next(1, 10),
-            SizeBytes = (ulong)Random.Shared.Next(1, 10),
-            MessagesCount = (ulong)Random.Shared.Next(1, 10),
-            Name = "Test Topic" + Random.Shared.Next(1, 69),
-            CreatedAt = DateTimeOffset.UtcNow,
-            TopicsCount = Random.Shared.Next(1, 10),
-            Topics = responses
-        };
-    }
     internal static StreamResponse CreateStreamsResponse()
     {
         return new StreamResponse
