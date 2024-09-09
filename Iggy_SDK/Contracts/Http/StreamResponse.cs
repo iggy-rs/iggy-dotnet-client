bae19dc95f9ab03881ec13d0aa115717086ddfaa
José Araújo <jroberto.araujo@gmail.com>
2024-09-09 12:25:56 +0100

    remove commented code

--- Iggy_SDK/Contracts/Http/StreamResponse.cs
+++ Iggy_SDK/Contracts/Http/StreamResponse.cs
@@ -3,11 +3,9 @@ namespace Iggy_SDK.Contracts.Http;
 
 public sealed class StreamResponse
 {
-    //{"id":940,"created_at":1725877995143672,"name":"test-stream33seze1n76bbpq","size":"0 B","messages_count":0,"topics_count":0,"topics":[]}
-    
     public required int Id { get; init; }
     public required string Name { get; init; }
-    public required ulong Size { get; init; }//
+    public required ulong Size { get; init; }
     public required DateTimeOffset CreatedAt { get; init; }
     public required ulong MessagesCount { get; init; }
     public required int TopicsCount { get; init; }
