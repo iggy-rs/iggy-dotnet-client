7b8c475db91c2f6e05dd2f0aa5e519621578d936
José Araújo <jroberto.araujo@gmail.com>
2024-09-24 14:43:38 +0100

    Fix UsersE2E tests to use HTTP and TCP protocols

--- Iggy_SDK_Tests/E2ETests/UsersE2E.cs
+++ Iggy_SDK_Tests/E2ETests/UsersE2E.cs
@@ -38,174 +38,121 @@ public sealed class UsersE2E : IClassFixture<IggyTcpUsersFixture>
     public async Task CreateUser_Duplicate_Should_Throw_InvalidResponse()
     {
         // act & assert
-        await _fixture.TcpSut.Invoking(y =>
-                y.CreateUser(UsersFixtureBootstrap.UserRequest)
+        var tasks = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
+        {
+            await sut.Invoking(async x =>
+                await x.CreateUser(UsersFixtureBootstrap.UserRequest)
             )
             .Should()
             .ThrowExactlyAsync<InvalidResponseException>();
+        }));
         
-        
-        // await _fixture.TcpSut.Invoking(y => y.CreateUser(UsersFixtureBootstrap.UserRequest))
-        //         .Should()
-        //         .ThrowExactlyAsync<InvalidResponseException>();
-        // TODO: This code block is commmented bacause TCP implementation is not working properly.
-        // var tasks = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
-        // {
-        //     await sut.Invoking(async x =>
-        //         await x.CreateUser(UsersFixtureBootstrap.UserRequest)
-        //     )
-        //     .Should()
-        //     .ThrowExactlyAsync<InvalidResponseException>();
-        // }));
-        //
-        // await Task.WhenAll(tasks);
+        await Task.WhenAll(tasks);
     }
     
     [Fact, TestPriority(3)]
     public async Task GetUser_Should_ReturnValidResponse()
     {
-        // act
-        var response = await _fixture.HttpSut.GetUser(Identifier.Numeric(2));
+        var tasks = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
+        {
+            // act
+            var response = await sut.GetUser(Identifier.Numeric(2));
+            
+            // assert
+            response.Should().NotBeNull();
+            response!.Id.Should().Be(2);
+            response.Username.Should().Be(UsersFixtureBootstrap.UserRequest.Username);
+            response.Status.Should().Be(UsersFixtureBootstrap.UserRequest.Status);
+            response.Permissions!.Global.Should().NotBeNull();
+            response.Permissions.Global.Should().BeEquivalentTo(UsersFixtureBootstrap.UserRequest.Permissions!.Global);
+        })).ToArray();
         
-        // assert
-        response.Should().NotBeNull();
-        response!.Id.Should().Be(2);
-        response.Username.Should().Be(UsersFixtureBootstrap.UserRequest.Username);
-        response.Status.Should().Be(UsersFixtureBootstrap.UserRequest.Status);
-        response.Permissions!.Global.Should().NotBeNull();
-        response.Permissions.Global.Should().BeEquivalentTo(UsersFixtureBootstrap.UserRequest.Permissions!.Global);
-        
-        // TODO: This code block is commmented bacause TCP implementation is not working properly.
-        // var tasks = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
-        // {
-        //     var response = await sut.GetUser(Identifier.Numeric(2));
-        //     response.Should().NotBeNull();
-        //     response!.Id.Should().Be(2);
-        //     response.Username.Should().Be(UsersFixtureBootstrap.UserRequest.Username);
-        //     response.Status.Should().Be(UsersFixtureBootstrap.UserRequest.Status);
-        //     response.Permissions!.Global.Should().NotBeNull();
-        //     response.Permissions.Global.Should().BeEquivalentTo(UsersFixtureBootstrap.UserRequest.Permissions!.Global);
-        // })).ToArray();
-        // await Task.WhenAll(tasks);
+        await Task.WhenAll(tasks);
     }
     
     [Fact, TestPriority(4)]
     public async Task GetUsers_Should_ReturnValidResponse()
     {
-        // act
-        var response = await _fixture.HttpSut.GetUsers();
+        var tasks = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
+        {
+            // act
+            var response = await sut.GetUsers();
+            
+            // assert
+            response.Should().NotBeEmpty();
+            response.Should().HaveCount(2);
+        })).ToArray();
         
-        // assert
-        response.Should().NotBeEmpty();
-        response.Should().HaveCount(2);
-        
-        // TODO: This code block is commmented bacause TCP implementation is not working properly.
-        // var tasks = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
-        // {
-        //     var response = await sut.GetUsers();
-        //     response.Should().NotBeEmpty();
-        //     response.Should().HaveCount(2);
-        // })).ToArray();
-        // await Task.WhenAll(tasks);
+        await Task.WhenAll(tasks);
     }
     
     [Fact, TestPriority(5)]
     public async Task UpdateUser_Should_UpdateUser_Successfully()
     {
-        await _fixture.HttpSut
-            .Invoking(y => 
-                y.UpdateUser(new UpdateUserRequest
-                        {
-                            UserId = Identifier.Numeric(2),
-                            Username = UsersFixtureBootstrap.NewUsername,
-                            UserStatus = UserStatus.Active
-                        })
-            )
-            .Should()
-            .NotThrowAsync();
-
-        var user = await _fixture.HttpSut.GetUser(Identifier.Numeric(2));
-        user!.Username.Should().Be(UsersFixtureBootstrap.NewUsername);
+        var tasks = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
+        {
+            //act
+            await sut.Invoking(async x =>
+                await x.UpdateUser(new UpdateUserRequest
+                {
+                    UserId = Identifier.Numeric(2), Username = UsersFixtureBootstrap.NewUsername, UserStatus = UserStatus.Active
+                })).Should().NotThrowAsync();
+            
+            var user = await sut.GetUser(Identifier.Numeric(2));
+            
+            // assert
+            user!.Username.Should().Be(UsersFixtureBootstrap.NewUsername);
+        })).ToArray();
         
-        // TODO: This code block is commmented bacause TCP implementation is not working properly.
-        // var tasks = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
-        // {
-        //     await sut.Invoking(async x =>
-        //         await x.UpdateUser(new UpdateUserRequest
-        //         {
-        //             UserId = Identifier.Numeric(2), Username = UsersFixtureBootstrap.NewUsername, UserStatus = UserStatus.Active
-        //         })).Should().NotThrowAsync();
-        //     var user = await sut.GetUser(Identifier.Numeric(2));
-        //     user!.Username.Should().Be(UsersFixtureBootstrap.NewUsername);
-        // })).ToArray();
-        // await Task.WhenAll(tasks);
+        await Task.WhenAll(tasks);
     }
     
     [Fact, TestPriority(6)]
     public async Task ChangePermissions_Should_ChangePermissions_Successfully()
     {
-        await _fixture.HttpSut
-            .Invoking(y => 
-                y.UpdatePermissions(new UpdateUserPermissionsRequest
-                    {
-                        UserId = Identifier.Numeric(2),
-                        Permissions = UsersFixtureBootstrap.UpdatePermissionsRequest
-                    })
-            )
-            .Should()
-            .NotThrowAsync();
+        var tasks = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
+        {
+            // act
+            await sut.Invoking(async x =>
+                await x.UpdatePermissions(new UpdateUserPermissionsRequest
+                {
+                    UserId = Identifier.Numeric(2), Permissions = UsersFixtureBootstrap.UpdatePermissionsRequest
+                })).Should().NotThrowAsync();
+            
+            var user = await sut.GetUser(Identifier.Numeric(2));
+            
+            // assert
+            user!.Permissions!.Global.Should().NotBeNull();
+            user.Permissions.Should().BeEquivalentTo(UsersFixtureBootstrap.UpdatePermissionsRequest);
+        })).ToArray();
         
-        var user = await _fixture.HttpSut.GetUser(Identifier.Numeric(2));
-        user!.Permissions!.Global.Should().NotBeNull();
-        user.Permissions.Should().BeEquivalentTo(UsersFixtureBootstrap.UpdatePermissionsRequest);
-        
-        // TODO: This code block is commmented bacause TCP implementation is not working properly.
-        // var tasks = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
-        // {
-        //     await sut.Invoking(async x =>
-        //         await x.UpdatePermissions(new UpdateUserPermissionsRequest
-        //         {
-        //             UserId = Identifier.Numeric(2), Permissions = UsersFixtureBootstrap.UpdatePermissionsRequest
-        //         })).Should().NotThrowAsync();
-        //     var user = await sut.GetUser(Identifier.Numeric(2));
-        //     user!.Permissions!.Global.Should().NotBeNull();
-        //     user.Permissions.Should().BeEquivalentTo(UsersFixtureBootstrap.UpdatePermissionsRequest);
-        // })).ToArray();
-        // await Task.WhenAll(tasks);
+        await Task.WhenAll(tasks);
     }
     
     [Fact, TestPriority(7)]
     public async Task DeleteUser_Should_DeleteUser_Successfully()
     {
-        // act & assert
-        await _fixture.HttpSut.Invoking(y => y.DeleteUser(Identifier.Numeric(2)))
-            .Should()
-            .NotThrowAsync();
+        var task = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
+        {
+            // act & assert
+            await sut.Invoking(async x =>
+                await x.DeleteUser(Identifier.Numeric(2))).Should().NotThrowAsync();
+        })).ToArray();
         
-        // TODO: This code block is commmented bacause TCP implementation is not working properly.
-        // var task = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
-        // {
-        //     await sut.Invoking(async x =>
-        //         await x.DeleteUser(Identifier.Numeric(2))).Should().NotThrowAsync();
-        // })).ToArray();
-        // await Task.WhenAll(task);
+        await Task.WhenAll(task);
     }
     
     [Fact, TestPriority(8)]
     public async Task LogoutUser_Should_LogoutUser_Successfully()
     {
-        // act & assert
-        await _fixture.HttpSut.Invoking(y => y.LogoutUser())
-            .Should()
-            .NotThrowAsync();
+        var tasks = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
+        {
+            // act & assert
+            await sut.Invoking(async x =>
+                await x.LogoutUser()).Should().NotThrowAsync();
+        })).ToArray();
         
-        // TODO: This code block is commmented bacause TCP implementation is not working properly.
-        // var tasks = _fixture.SubjectsUnderTest.Select(sut => Task.Run(async () =>
-        // {
-        //     await sut.Invoking(async x =>
-        //         await x.LogoutUser()).Should().NotThrowAsync();
-        // })).ToArray();
-        // await Task.WhenAll(tasks);
+        await Task.WhenAll(tasks);
     }
 }
 
